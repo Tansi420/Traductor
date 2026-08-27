cdb3aca15c2b8038f603d883968c0b23ca0b17a5
Tansi420 <127781271+Tansi420@users.noreply.github.com>
2026-08-27 13:13:59 -0500

    Update s_t.py

--- s_t.py
+++ s_t.py
@@ -19,7 +19,7 @@ st.title("TRADUCTOR.")
 st.subheader("Escucho lo que quieres traducir.")
 
 
-image = Image.open('OIG7.jpg')
+image = Image.open('3d152300d0885811aa6567e103d23927.jpg')
 
 st.image(image,width=300)
 with st.sidebar:
